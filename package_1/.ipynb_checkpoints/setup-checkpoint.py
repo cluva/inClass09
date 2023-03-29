01da02d00c974015a4ea632cca6000f9958858f8
Your Name <you@example.com>
2023-03-28 20:31:35 -0400

    add package

---
+++ package_1/.ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = 'hello_world',
+     version = '1',
+     description = 'in class',
+     url = 'https://github.com/cluva/inClass09.git',
+     license = 'MIT',
+     packages = ['hello_world'])
